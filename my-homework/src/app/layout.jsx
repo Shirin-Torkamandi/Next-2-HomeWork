5d0acf62bdbef080f8cf01ae9d53052965035f79
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-13 17:29:06 +0330

    updating layout.jsx

--- my-homework/src/app/layout.jsx
+++ my-homework/src/app/layout.jsx
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { CssBaseline } from "@mui/material";
+import Header from "@/components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,6 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <CssBaseline />
+        <Header />
         {children}
       </body>
     </html>
